ef1a7f0eaccf89dac5085ef75772c9d47fc0b383
aespinoza777 <63624533+aespinoza777@users.noreply.github.com>
2020-12-22 20:28:28 -0600

    Update Article.rb

--- Article.rb
+++ Article.rb
@@ -1,21 +1,21 @@
 class Article
-    attr_accessor :content
-    attr_reader :title, :description, :url
-    @@all = []
-  
-    def initialize(title, description, url)
-      @title = title
-      @description = description
-      @url = url
-      @@all << self
-    end
-  
-    def self.all
-      @@all
-    end
-  
-    def self.destroy_all
-      @@all = []
-    end
-  
+  attr_accessor :content
+  attr_reader :title, :description, :url
+  @@all = []
+
+  def initialize(title, description, url)
+    @title = title
+    @description = description
+    @url = url
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
+end
