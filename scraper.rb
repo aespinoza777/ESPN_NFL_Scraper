134fb91c9fc8c4bb59bcba3afb74afde962b0ced
aespinoza777 <63624533+aespinoza777@users.noreply.github.com>
2020-12-22 20:24:17 -0600

    Update scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -1,29 +1,45 @@
 class Scraper
 
-  
-    def self.scrape_new_nfl_articles(url)
-      html = open(url)
-      doc = Nokogiri::HTML(html)
-      stories = doc.css("article").select{|story| story.css('a.contentItem__content').attribute('href')}
-      stories.each do |story|
-        title = story.css("h2.contentItem__title").text
-        description = nil
-        if story.css('a.contentItem__content').attribute('href').value.start_with?("http")
-          url = story.css('a.contentItem__content').attribute('href').value
-        else
-          url = "https://www.espn.com#{story.css('a.contentItem__content').attribute('href').value}"
-        end
-        Article.new(title, description, url)
+  def self.scrape_new_articles(url)
+    html = open(url)
+    doc = Nokogiri::HTML(html)
+    stories = doc.css("div.item-info-wrap")
+    stories.each do |story|
+      title = story.css("h1").text
+      description = story.css("p").text
+      if story.css("a").attribute("href").value.start_with?("http")
+        url = story.css("a").attribute("href").value
+      else
+        url = "https://www.espn.com#{story.css("a").attribute("href").value}"
       end
-      Article.all
+      Article.new(title, description, url)
     end
-  
-    def self.get_content(article)
-      html = open(article.url)
-      doc = Nokogiri::HTML(html)
-      #for ESPN Radio links, this will return an empty array
-      article.content = doc.css("div.article-body p")
-      article.content = doc.css("div.Story__Body p") if article.content.size == 0
-    end
-  
+    Article.all
   end
+
+  def self.scrape_new_nhl_articles(url)
+    html = open(url)
+    doc = Nokogiri::HTML(html)
+    stories = doc.css("article").select{|story| story.css('a.contentItem__content').attribute('href')}
+    stories.each do |story|
+      title = story.css("h2.contentItem__title").text
+      description = nil
+      if story.css('a.contentItem__content').attribute('href').value.start_with?("http")
+        url = story.css('a.contentItem__content').attribute('href').value
+      else
+        url = "https://www.espn.com#{story.css('a.contentItem__content').attribute('href').value}"
+      end
+      Article.new(title, description, url)
+    end
+    Article.all
+  end
+
+  def self.get_content(article)
+    html = open(article.url)
+    doc = Nokogiri::HTML(html)
+    #for ESPN Radio links, this will return an empty array
+    article.content = doc.css("div.article-body p")
+    article.content = doc.css("div.Story__Body p") if article.content.size == 0
+  end
+
+end
